eeef119aa7d03efa611f4d3bed08924aceebc390
Pranav Patel <101977910+alloc7260@users.noreply.github.com>
2023-05-26 22:11:32 +0530

    added Corporate Actions Scraper

---
+++ Corporate Actions/Corporate Actions.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""### Import Dependencies"""
+
+import urllib
+import requests
+import pandas as pd
+import json
+
+"""### Define Helper Functions """
+
+def get_headers():
+    return {
+        "Host": "www.nseindia.com",
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:85.0) Gecko/20100101 Firefox/85.0",
+        "Accept": "*/*",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Accept-Encoding": "gzip, deflate, br",
+        "X-Requested-With": "XMLHttpRequest",
+        "DNT": "1",
+        "Connection": "keep-alive",
+    }
+
+def get_corpinfo(start_date, end_date, symbol=None):
+    """
+    Create threads for different requests, parses data, combines them and returns dataframe
+    Args:
+        start_date (datetime.datetime): start date
+        end_date (datetime.datetime): end date
+        symbol (str, optional): stock symbol. Defaults to None. TODO: implement for index`
+    Returns:
+        Pandas DataFrame: df containing data for symbol of provided date range
+    """
+    cookies = get_cookies()
+    params = {
+        "symbol": symbol,
+        "from_date": start_date,
+        "to_date": end_date,
+        "index": "equities",
+    }
+    # base_url = base_url
+    base_url = "https://www.nseindia.com/"
+    equity_corpinfo = "api/corporates-corporateActions?"
+    price_api = equity_corpinfo
+    url = base_url + price_api + urllib.parse.urlencode(params)
+    return fetch_url(url, cookies)
+
+def get_cookies():
+    base_url = "https://www.nseindia.com/"
+    response = requests.get(base_url, timeout=30, headers=get_headers())
+    if response.status_code != requests.codes.ok:
+        raise ValueError("Retry again in a minute.")
+    return response.cookies.get_dict()
+
+
+def fetch_url(url, cookies):
+    response = requests.get(url, timeout=30, headers=get_headers(), cookies=cookies)
+    if response.status_code == requests.codes.ok:
+        json_response = json.loads(response.content)
+        try:
+            return pd.DataFrame.from_dict(json_response["data"])
+        except:
+            return pd.DataFrame.from_dict(json_response)
+    else:
+        raise ValueError("Please try again in a minute.")
+
+"""### Scrape Directly to DataFrame """
+
+start_date = "01-03-2020"
+end_date = "26-05-2023"
+print(get_corpinfo(start_date, end_date, symbol="TCS"))
---
+++ Corporate Actions/README.md
@@ -0,0 +1,13 @@
+# NSE
+
+⚠ CAUTION :
+
+This lib uses the https://www.nseindia.com/api/ which is strictly disallow by nseindia for all the user-agents.
+
+Please use this lib only for Educational & Research purposes.
+
+I am not liable for any future disputes with nseindia.
+
+So use it at your own risk.
+
+Source : https://www.nseindia.com/robots.txt
